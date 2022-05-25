6c96bc625937bd97d3c150430cde95f8a12fc0cb
Mylo L <100321671+MSLigg@users.noreply.github.com>
2022-05-24 23:23:07 -0400

    i think it finally works

--- dodgeRam.js
+++ dodgeRam.js
@@ -1,22 +1,28 @@
 //this includes the vehicle class as a module
-const Vehicle = require("./vehicle").Vehicle
+const VehicleModule = require("./vehicle")
 
-class Car extends Vehicle {
+//this shows how to call from this module...
+let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
+console.log(v.make)
+
+class Truck extends VehicleModule {
     constructor(make, model, year, color, mileage) {
         super(make, model, year, color, mileage);
-        this.maxPassengers = 5;
-        this.passenger = 0;
-        this.numberOfWheels = 4;
-        this.maxSpeed = 160;
-        this.fuel = 10;
-        this.scheduleService = false;
+        this.maxPassengers =3;
+        this.passenger =0;
+        this.numberOfWheels =4;
+        this.maxSpeed =200;
+        this.fuel =10;
+        this.scheduleService = true;
     }
+
     checkService() {
-        if (this.mileage > 30000) {            
+        if (this.mileage > 50000) {            
             this.scheduleService = true
             return this.scheduleService;                       
         }
     }
+
     start() {
         if (this.fuel > 0) {            
             console.log('engine has started.');
@@ -26,6 +32,7 @@ class Car extends Vehicle {
             return this.started = false;
         }
     }
+
     loadPassenger(num) {
         if (this.passenger<this.maxPassengers) {
             if ((num + this.passenger) <= this.maxPassengers) {
@@ -33,15 +40,16 @@ class Car extends Vehicle {
                 return this.passenger;               
             } else {
                 console.log(this.model + " " + this.make + " don't have enough space to take all passengers.");
+
             }
         } else {
             console.log(this.model + " " + this.make + " is full");
         }
     }
-}
-let myCar = new Car('Chevy', 'Spark', '2016', 'Azure', 72000)
-    myCar.start()
-    myCar.loadPassenger(7)
-    myCar.stop()
-    myCar.checkService()
-console.log(myCar)
+
+let myTruck = new Truck('Dodge', 'RAM', '2022', 'Silver', 50000)
+    myTruck.start()
+    myTruck.loadPassenger(3)
+    myTruck.stop()
+    myTruck.checkService()
+console.log(myTruck)
